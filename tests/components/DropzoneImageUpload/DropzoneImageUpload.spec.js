e6f01e994c7386a9dd0759a7f4c556d01ad4b42d
ttbarnes <tony@tonybarnes.me>
2017-07-06 08:35:49 +0100

    DropzoneImageUpload unit test

---
+++ tests/components/DropzoneImageUpload/DropzoneImageUpload.spec.js
@@ -0,0 +1,25 @@
+import React from 'react'
+import DropzoneImageUpload from 'components/DropzoneImageUpload/DropzoneImageUpload'
+import { shallow } from 'enzyme'
+
+describe('(Component) DropzoneImageUpload', () => {
+  let wrapper,
+      props = {
+        title: 'my title',
+        input: {
+          name: 'something'
+        },
+        multiple: false
+      }
+
+  beforeEach(() => {
+    wrapper = shallow(<DropzoneImageUpload {...props} />)
+  });
+
+  it('should render <Dropzone /> with correct props', () => {
+    const actual = wrapper.containsMatchingElement(
+      <Dropzone name={props.input.name} multiple={props.multiple} />
+    );
+    expect(actual).to.equal(true);
+  });
+});
