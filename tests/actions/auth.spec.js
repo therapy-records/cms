6869c3c3e6bc1c218eafb701ad425c647a3cfecd
ttbarnes <tony@tonybarnes.me>
2017-08-13 09:52:56 +0100

    fix auth actions tests

--- tests/actions/auth.spec.js
+++ tests/actions/auth.spec.js
@@ -1,51 +1,52 @@
 import 'core-js';
-// import _axiosAuthHeaders from 'utils/axios'
-// import configureMockStore from 'redux-mock-store';
-// import thunk from 'redux-thunk';
+// import axios from 'axios';
+import _axiosAuthHeaders from 'utils/axios'
+import configureMockStore from 'redux-mock-store';
+import thunk from 'redux-thunk';
 import nock from 'nock';
-// import {
-  // API_ROOT,
-  // AUTH
+import {
+  API_ROOT,
+  AUTH
   // AUTH_LOGIN
-// } from 'constants';
-// import {
-//   USER_AUTH_SUCCESS,
-//   USER_AUTH_ERROR
-// } from 'constants/actions'
+} from 'constants';
+import {
+  USER_AUTH_SUCCESS,
+  USER_AUTH_ERROR
+} from 'constants/actions'
 import { authCheck, routeAuthCheck } from 'actions/auth';
 
-// const middlewares = [thunk];
-// const mockStore = configureMockStore(middlewares);
+const middlewares = [thunk];
+const mockStore = configureMockStore(middlewares);
 
-// const mock = {
-//   authResponseSuccess: {
-//     data: {
-//       success: true
-//     }
-//   },
-//   authResponseError: {
-//     data: {
-//       success: false
-//     }
-//   },
-//   loginForm: {
-//     username: 'admin',
-//     password: 'password'
-//   }
-// };
-// const mockState = {
-//   form: {
-//     LOGIN_FORM: {
-//       values: mock.loginForm
-//     }
-//   }
-// };
-// const mockDispatch = {
-//   authSuccess: () => {},
-//   authError: () => {}
-// }
+const mock = {
+  authResponseSuccess: {
+    data: {
+      success: true
+    }
+  },
+  authResponseError: {
+    data: {
+      success: false
+    }
+  },
+  loginForm: {
+    username: 'admin',
+    password: 'password'
+  }
+};
+const mockState = {
+  form: {
+    LOGIN_FORM: {
+      values: mock.loginForm
+    }
+  }
+};
+const mockDispatch = {
+  authSuccess: () => {},
+  authError: () => {}
+}
 
-// const store = mockStore(mockState, mockDispatch);
+const store = mockStore(mockState, mockDispatch);
 
 describe('(Actions) auth', () => {
   describe('(Thunk) authCheck', () => {
@@ -63,39 +64,41 @@ describe('(Actions) auth', () => {
 
     // todo: why getting 'network error' issues with nock/axios?
 
-    // it('should dispatch the correct actions on auth success', () => {
-    //   _axiosAuthHeaders.post = sinon.stub().returns(Promise.resolve(mock.authResponseSuccess));
-    //   nock(API_ROOT + AUTH)
-    //     .post('/auth')
-    //     .reply(200, mock.authResponseSuccess);
+    it('should dispatch the correct actions on auth success', () => {
+      _axiosAuthHeaders.post = sinon.stub().returns(Promise.resolve(mock.authResponseSuccess));
+      nock(API_ROOT + AUTH)
+        .post('/auth')
+        .reply(200, mock.authResponseSuccess);
 
-    //   const expectedActions = [
-    //     { type: USER_AUTH_SUCCESS, payload: { isAuth: true } }
-    //   ];
+      const expectedActions = [
+        { type: USER_AUTH_ERROR, payload: { isAuth: false } },
+        { type: USER_AUTH_SUCCESS, payload: { isAuth: true } }
+      ];
 
-    //   return store.dispatch(authCheck()).then(() => {
-    //     const storeActions = store.getActions();
-    //     expect(storeActions).to.deep.equal(expectedActions);
-    //     store.clearActions();
-    //   });
-    // });
+      return store.dispatch(authCheck()).then(() => {
+        const storeActions = store.getActions();
+        expect(storeActions).to.deep.equal(expectedActions);
+        store.clearActions();
+      });
+    });
 
-    // it('should dispatch the correct actions on auth error', () => {
-    //   _axiosAuthHeaders.post = sinon.stub().returns(Promise.resolve(mock.authResponseError));
-    //   nock(API_ROOT + AUTH)
-    //     .post('/auth')
-    //     .reply(200, mock.authResponseError);
+    it('should dispatch the correct actions on auth error', () => {
+      _axiosAuthHeaders.post = sinon.stub().returns(Promise.resolve(mock.authResponseError));
+      nock(API_ROOT + AUTH)
+        .post('/auth')
+        .reply(200, mock.authResponseError);
 
-    //   const expectedActions = [
-    //     { type: USER_AUTH_ERROR, payload: { isAuth: false } }
-    //   ];
+      const expectedActions = [
+        { type: USER_AUTH_ERROR, payload: { isAuth: false } },
+        { type: USER_AUTH_ERROR, payload: { isAuth: false } }
+      ];
 
-    //   return store.dispatch(authCheck()).then(() => {
-    //     const storeActions = store.getActions();
-    //     expect(storeActions).to.deep.equal(expectedActions);
-    //     store.clearActions();
-    //   });
-    // });
+      return store.dispatch(authCheck()).then(() => {
+        const storeActions = store.getActions();
+        expect(storeActions).to.deep.equal(expectedActions);
+        store.clearActions();
+      });
+    });
   });
 
   describe('(Thunk) routeAuthCheck', () => {
@@ -111,7 +114,7 @@ describe('(Actions) auth', () => {
       expect(routeAuthCheck()).to.be.a('function');
     });
 
-    // todo: why getting 'network error' issues with nock/axios?
+    // todo: how to get store/disaptch working with routeAuthCheck?
 
     // it('should dispatch the correct actions on auth success', () => {
     //   axios.create().post = sinon.stub().returns(Promise.resolve(mock.authResponseSuccess));
@@ -120,10 +123,12 @@ describe('(Actions) auth', () => {
     //     .reply(200, mock.authResponseSuccess.data);
 
     //   const expectedActions = [
+    //     { type: USER_AUTH_ERROR, payload: { isAuth: true } },
     //     { type: USER_AUTH_SUCCESS, payload: { isAuth: true } }
     //   ];
 
-    //   return store.dispatch(routeAuthCheck()).then(() => {
+    //   const callRouteAuthCheck = routeAuthCheck(mockStore, mockState, () => {}, () => {});
+    //   return store.dispatch(callRouteAuthCheck()).then(() => {
     //     const storeActions = store.getActions();
     //     expect(storeActions).to.deep.equal(expectedActions);
     //     store.clearActions();
