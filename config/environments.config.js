ddc49cb26fc398bda9fae72d0aa9b40632e1ac0b
ttbarnes <tony@tonybarnes.me>
2017-10-05 13:41:08 +0100

    API_ROOT env

--- config/environments.config.js
+++ config/environments.config.js
@@ -9,15 +9,13 @@ module.exports = {
   // are served webpack by to fix this issue:
   // http://stackoverflow.com/questions/34133808/webpack-ots-parsing-error-loading-fonts/34133809#34133809
   development : (config) => ({
-    compiler_public_path : `http://${config.server_host}:${config.server_port}/`,
-    API_ROOT: 'http://localhost:4040/api/'
+    compiler_public_path : `http://${config.server_host}:${config.server_port}/`
   }),
 
   // ======================================================
   // Overrides when NODE_ENV === 'production'
   // ======================================================
   production : (config) => ({
-    API_ROOT: 'https://fr-cms-api.herokuapp.com/api/',
     compiler_public_path     : '/',
     compiler_fail_on_warning : false,
     compiler_hash_type       : 'chunkhash',
--- src/constants/index.js
+++ src/constants/index.js
@@ -1,5 +1,8 @@
-// export const API_ROOT = 'http://localhost:4040/api/';
-export const API_ROOT = 'https://fr-cms-api.herokuapp.com/api/';
+const envProd = process.env === 'production';
+export const API_ROOT_DEV = 'http://localhost:4040/api/';
+export const API_ROOT_PROD = 'https://fr-cms-api.herokuapp.com/api/';
+
+export const API_ROOT = envProd ? API_ROOT_PROD : API_ROOT_DEV;
 export const NEWS = 'news';
 export const NEWS_CREATE = 'news/create';
 export const NEWS_QUEUE = 'news/queue';
