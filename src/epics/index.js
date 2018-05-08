be163422149a9a6861f82c78d03f41eea49f272a
ttbarnes <tony@tonybarnes.me>
2018-05-08 09:05:07 +0100

    extract uiState constants

--- src/epics/index.js
+++ src/epics/index.js
@@ -2,11 +2,15 @@ import { combineEpics } from 'redux-observable';
 import 'rxjs/add/operator/filter';
 import 'rxjs/add/operator/mapTo';
 import { authError } from '../reducers/user';
+import {
+  UISTATE_PROMISE_ERROR
+} from '../constants/actions'
+
 
 const authCheckEpic = (action$, store) => {
   return action$
     .filter(action =>
-        action.type === 'UISTATE_PROMISE_ERROR' &&
+        action.type === UISTATE_PROMISE_ERROR &&
         action.payload === 401
     )
     .mapTo(authError());
