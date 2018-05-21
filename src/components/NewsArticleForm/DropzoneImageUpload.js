523855084ad1e480221e2951d09e125ba366d72f
ttbarnes <tony@tonybarnes.me>
2018-05-21 08:43:24 +0100

    imageUpload - clear errors on retry

--- src/components/NewsArticleForm/DropzoneImageUpload.js
+++ src/components/NewsArticleForm/DropzoneImageUpload.js
@@ -45,6 +45,9 @@ export class DropzoneImageUpload extends React.Component {
 
   // todo: redux, promisify
   uploadSingleImage(file) {
+    this.setState({
+      invalidDimensions: []
+    });
     let upload = request.post(CLOUDINARY_UPLOAD_URL)
                         .field('upload_preset', CLOUDINARY_UPLOAD_PRESET_ID)
                         .field('file', file);
