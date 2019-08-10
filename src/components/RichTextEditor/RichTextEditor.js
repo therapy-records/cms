a46e9cf3c2fd32ec8ef525dda81362e0e2cb9bbb
ttbarnes <tony@tonybarnes.me>
2019-08-10 09:58:02 +0100

    custom RichTextEditor toolbar

--- src/components/RichTextEditor/RichTextEditor.js
+++ src/components/RichTextEditor/RichTextEditor.js
@@ -3,6 +3,19 @@ import PropTypes from 'prop-types'
 import RichTextEditor from 'react-rte'
 import './RichTextEditor.css'
 
+const RTE_TOOLBAR_CONFIG = {
+  display: ['INLINE_STYLE_BUTTONS', 'LINK_BUTTONS', 'BLOCK_TYPE_BUTTONS'],
+  INLINE_STYLE_BUTTONS: [
+    { label: 'Bold', style: 'BOLD', className: 'custom-css-class' },
+    { label: 'Italic', style: 'ITALIC' },
+    { label: 'Underline', style: 'UNDERLINE' }
+  ],
+  BLOCK_TYPE_BUTTONS: [
+    { label: 'UL', style: 'unordered-list-item' },
+    { label: 'blockquote', style: 'blockquote' }
+  ]
+};
+
 class _RichTextEditor extends React.Component {
   constructor(props){
     super(props);
@@ -41,6 +54,7 @@ class _RichTextEditor extends React.Component {
       {touched && error && (<p>Copy is {error}</p>)}
 
         <RichTextEditor
+          toolbarConfig={RTE_TOOLBAR_CONFIG}
           value={this.state.value}
           placeholder='This month has been fantastic...'
           onChange={this.handleOnChange}
