7dd0dc14be97665e431b10cb8f64b46d3b337a69
ttbarnes <tony@tonybarnes.me>
2019-12-21 12:13:14 +0000

    prevent typename from being returned in graphql responses

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom';
 import { createBrowserHistory } from 'history';
 import { ApolloProvider } from 'react-apollo';
 import ApolloClient from 'apollo-boost';
+import { InMemoryCache } from 'apollo-cache-inmemory';
 import Routes from './routes';
 import store from './store';
 import { GRAPHQL_ROOT } from './constants';
@@ -11,7 +12,10 @@ const DOM_ROOT_ID = 'root';
 const DOM_ROOT = document.getElementById(DOM_ROOT_ID);
 
 const client = new ApolloClient({
-  uri: GRAPHQL_ROOT
+  uri: GRAPHQL_ROOT,
+  cache: new InMemoryCache({
+    addTypename: false
+  })
 });
 
 ReactDOM.render(
