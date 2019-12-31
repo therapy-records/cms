4047ffb9977a28ede58fa852ddcd46991e6a7eac
ttbarnes <tony@tonybarnes.me>
2019-12-31 12:39:21 +0000

    form error handling poc

--- src/components/Form/index.js
+++ src/components/Form/index.js
@@ -8,7 +8,14 @@ const Form = ({
   mutation,
   isEditForm
 }) => {
-  const [postForm] = useMutation(mutation);
+
+  const [
+    postForm,
+    {
+      loading,
+      error
+    }
+  ] = useMutation(mutation);
 
   const handleSubmit = (ev) => {
     ev.preventDefault();
@@ -16,28 +23,51 @@ const Form = ({
     const postData = handleFormData(form);
     postData.avatarUrl = 'https://via.placeholder.com/250'; // TEMP
 
-    console.log('post data \n', postData);
     postForm({
-      variables: { input: postData }
-    });
+      variables: { input: postData },
+      errorPolicy: 'all'
+    }).then(
+      result => {
+        console.log('*** success');
+      },
+      (errors) => {
+        console.log('*** errors ', errors);
+      }
+    )
+    
   }
 
   const submitButtonCopy = isEditForm ? 'Update' : 'Add';
 
   return (
-    <form onSubmit={handleSubmit} encType='multipart/form-data'>
-
-      {children}
-
-      <div className='row-large'>
-        <button
-          type='submit'
-          className='btn-lg btn-submit'
-        >{submitButtonCopy}
-        </button>
+    <div>
+      <div style={{
+        position: 'fixed',
+        top: '0',
+        background: '#EEE',
+        color: '#000',
+        padding: '1em',
+        width: '100%',
+        zIndex: '9999'
+      }}>
+        {loading && <p>loading</p>}
+        {error && error.message && error.message && <p>error! {error.message}</p>}
       </div>
 
-    </form>
+      <form onSubmit={handleSubmit} encType='multipart/form-data'>
+
+        {children}
+
+        <div className='row-large'>
+          <button
+            type='submit'
+            className='btn-lg btn-submit'
+          >{submitButtonCopy}
+          </button>
+        </div>
+
+      </form>
+    </div>
   )
 };
 
