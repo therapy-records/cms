6e5f6c9ce3fc1718a1b72d514a3e879dd4f422b6
ttbarnes <tony@tonybarnes.me>
2020-01-10 08:45:56 +0000

    use FormSuccess in News and Journalism forms

--- src/routes/News/ArticleEdit/index.js
+++ src/routes/News/ArticleEdit/index.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import { Link } from 'react-router-dom';
 import { connect } from 'react-redux';
 import { resetPromiseState } from '../../../actions/uiState';
 import { editNews, editNewsSuccess } from '../../../actions/news';
