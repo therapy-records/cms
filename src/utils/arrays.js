1b9a59559a67609268ccc6b040a2929eca3a8d21
ttbarnes <tony@tonybarnes.me>
2020-01-16 10:42:37 +0000

    arrayOfStringsHasValues function

--- src/utils/arrays.js
+++ src/utils/arrays.js
@@ -1,5 +1,15 @@
 import { isEmptyString } from './strings';
 
+export const arrayOfStringsHasValues = (arr=[]) => {
+  const itemsWithValues = arr.filter(str =>
+    str && !isEmptyString(str)
+  );
+  if (itemsWithValues.length >= 1) {
+    return true;
+  }
+  return false;
+}
+
 export const arrayOfObjectsHasValues = (arr=[]) => {
   const itemsWithValues = arr.filter(i =>
     i.value && !isEmptyString(i.value)
--- src/utils/arrays.test.js
+++ src/utils/arrays.test.js
@@ -1,8 +1,28 @@
 import {
+  arrayOfStringsHasValues,
   arrayOfObjectsHasValues
 } from './arrays';
 
 describe('(Utils) arrays', () => {
+
+  describe('arrayOfStringsHasValues', () => {
+
+    it('should return false when items are empty strings', () => {
+      const mock = [ '', "", '' ];
+      expect(arrayOfStringsHasValues(mock)).to.eq(false);
+    });
+
+    it('should return true when a single string exists', () => {
+      const mock = [ '', 'test', '', ];
+      expect(arrayOfStringsHasValues(mock)).to.eq(true);
+    });
+
+    it('should return true when multiple objects in the array have a value', () => {
+      const mock = [ '', 'test', 'testing', {}, '', ];
+      expect(arrayOfStringsHasValues(mock)).to.eq(true);
+    });
+
+  });
 
   describe('arrayOfObjectsHasValues', () => {
 
