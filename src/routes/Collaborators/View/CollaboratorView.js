c0990ac31996a9a1605d4f7e1546169dd7391657
ttbarnes <tony@tonybarnes.me>
2020-01-16 13:03:31 +0000

    CollaboratorView using SingleEntityContainer

--- src/routes/Collaborators/View/CollaboratorView.js
+++ src/routes/Collaborators/View/CollaboratorView.js
@@ -1,106 +1,33 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import {
-  useQuery,
-  useMutation
-} from '@apollo/react-hooks';
 import { GET_COLLABORATOR } from '../../../queries';
 import { DELETE_COLLABORATOR } from '../../../mutations';
-import ArticleHeader from '../../../components/ArticleHeader';
-import LoadingSpinner from '../../../components/LoadingSpinner';
-import StickyNew from '../../../components/StickyNew';
+import SingleEntityContainer from '../../../containers/SingleEntityContainer';
 import Collaborator from './Collaborator';
 import './CollaboratorView.css';
-import FormSuccess from '../../../components/FormElements/FormSuccess';
 
-const CollaboratorView = ({ match }) => {
-  const { id: collabId } = match.params;
-
-  const {
-    loading: queryLoading,
-    error: queryError,
-    data
-  } = useQuery(GET_COLLABORATOR, {
-    variables: {
-      id: collabId
-    }
-  });
-
-  const [
-    deleteCollaborator,
-    {
-      loading: mutationLoading,
-      error: mutationError,
-      data: deletedData
-    }
-  ] = useMutation(DELETE_COLLABORATOR);
-
-  const handleOnDelete = (mutation, id) => {
-    mutation({
-      variables: { id }
-    })
-  }
-
-  const hasData = (data && data.collaborator);
-  const deleteSuccess = (deletedData && deletedData.deleteCollaborator._id);
-  const isLoading = (queryLoading || mutationLoading);
-  const hasError = (queryError || mutationError);
-
-  const renderPageContent = (hasData &&
-                             !isLoading &&
-                             !deleteSuccess);
+const CollaboratorViewNew = ({
+  match
+}) => {
+  const { id } = match.params;
 
   return (
-    <article className='container'>
-
-      {isLoading && (
-        <LoadingSpinner
-          active
-          fullScreen
-        />
-      )}
-
-      {deleteSuccess && (
-        <FormSuccess
-          baseUrl='/collaborators'
-          copy={{
-            success: 'Successfully Deleted',
-            homeLink: 'Go to Collaborators'
-          }}
-        />
-      )}
-
-      {hasError && (
-        <StickyNew>
-          <p>Sorry, something has gone wrong.</p>
-        </StickyNew>
-      )}
-
-      {renderPageContent && (
-        <div>
-
-          <ArticleHeader
-            baseUrl='/collaborators'
-            article={{}}
-            onDeleteArticle={() => handleOnDelete(deleteCollaborator, data.collaborator._id)}
-            heading={data.collaborator.name}
-            showDeleteButton
-          />
-
-          <Collaborator {...data.collaborator} />
-
-        </div>
-      )}
-
-    </article>
+    <SingleEntityContainer
+      baseUrl='/collaborators'
+      entityName='collaborator'
+      id={id}
+      component={Collaborator}
+      query={GET_COLLABORATOR}
+      mutation={DELETE_COLLABORATOR}
+      renderEditLink
+    />
   );
-};
+}
 
-
-CollaboratorView.propTypes = {
+CollaboratorViewNew.propTypes = {
   match: PropTypes.shape({
     params: PropTypes.object
   })
 };
 
-export default CollaboratorView;
+export default CollaboratorViewNew;
--- src/routes/Collaborators/View/CollaboratorView.test.js
+++ src/routes/Collaborators/View/CollaboratorView.test.js
@@ -1,174 +1,43 @@
 import React from 'react';
-import Enzyme, { mount } from 'enzyme';
+import Enzyme, { shallow } from 'enzyme';
 import Adapter from 'enzyme-adapter-react-16';
-import { act } from 'react-dom/test-utils';
-import { BrowserRouter } from 'react-router-dom';
-import { MockedProvider } from '@apollo/react-testing';
 import { GET_COLLABORATOR } from '../../../queries';
 import { DELETE_COLLABORATOR } from '../../../mutations';
-import CollaboratorView from './index';
-import LoadingSpinner from '../../../components/LoadingSpinner';
-import StickyNew from '../../../components/StickyNew';
+import CollaboratorView from './CollaboratorView';
+import SingleEntityContainer from '../../../containers/SingleEntityContainer';
 import Collaborator from './Collaborator';
 
 Enzyme.configure({ adapter: new Adapter() });
 
-let mocks = [
-  {
-    request: {
-      query: GET_COLLABORATOR,
-      variables: { id: '1234' }
-    },
-    result: {
-      data: {
-        collaborator: {
-          _id: '1234',
-          name: 'test',
-          role: 'testing',
-          about: '<p>test</p>',
-          avatarUrl: 'test.com',
-          urls: {
-            website: 'test',
-            facebook: 'test',
-            instagram: 'test',
-            twitter: 'test',
-            soundcloud: 'test',
-            bio: 'test',
-            email: 'test@test.com',
-            phone: '0123456789',
-            other: []
-          },
-          collabOn: []
-        }
-      }
-    }
-  },
-  {
-    request: {
-      query: DELETE_COLLABORATOR,
-      variables: { id: '1234' }
-    },
-    result: {
-      data: {
-        _id: '1234'
-      }
-    }
-  }
-];
-
 describe('(Component) CollaboratorView', () => {
-  let wrapper,
-      props = {
-        match: {
-          params: {
-            id: '1234'
-          }
-        }
-      };
+  let wrapper;
+  const props = {
+    match: {
+      params: {
+        id: '1234'
+      }
+    }
+  };
 
-  const actions = async(wrapper, _actions) => {
-    await act(async() => {
-      await (new Promise(resolve => setTimeout(resolve, 0)));
-      _actions();
-      wrapper.update();
-    });
-  }
-
-  describe('when there are no errors', () => {
-    beforeEach(() => {
-      wrapper = mount(
-        <BrowserRouter>
-          <MockedProvider mocks={mocks} addTypename={false}>
-            <CollaboratorView {...props} />
-          </MockedProvider>
-        </BrowserRouter>
-      );
-    })
-
-    it('should render a page title', async() => {
-      await actions(wrapper, () => {
-        wrapper.update();
-        const actual = wrapper.containsMatchingElement(
-          <h2>{mocks[0].result.data.collaborator.name}</h2>
-        );
-        expect(actual).to.equal(true);
-      });
-    });
-    
-    it('should render <ArticleHeader />', async() => {
-      await actions(wrapper, () => {
-        wrapper.update();
-        const articleHeader = wrapper.find('ArticleHeader');
-        expect(articleHeader.prop('baseUrl')).to.eq('/collaborators');
-        expect(articleHeader.prop('article')).to.deep.eq({});
-        expect(articleHeader.prop('onDeleteArticle')).to.be.a('function');
-        expect(articleHeader.prop('heading')).to.eq(mocks[0].result.data.collaborator.name);
-        expect(articleHeader.prop('showDeleteButton')).to.eq(true);
-      });
-    });
-
-    it('should render <Collaborator />', async() => {
-      await actions(wrapper, () => {
-        wrapper.update();
-        const actual = wrapper.containsMatchingElement(
-          <Collaborator {...mocks[0].result.data.collaborator} />
-        );
-        expect(actual).to.equal(true);
-      });
-    });
-
+  beforeEach(() => {
+    wrapper = shallow(
+      <CollaboratorView {...props} />
+    );
   });
 
-  describe('when the graphQL query is loading', () => {
-
-    it('should render <LoadingSpinner />', async() => {
-      wrapper = mount(
-        <BrowserRouter>
-          <MockedProvider mocks={mocks} addTypename={false}>
-            <CollaboratorView {...props} />
-          </MockedProvider>
-        </BrowserRouter>
-      );
-
-      await actions(wrapper, () => {
-        const actual = wrapper.containsMatchingElement(
-          <LoadingSpinner
-            active
-            fullScreen
-          />
-        );
-        expect(actual).to.equal(true);
-      });
-    });
-  });
-
-  describe('when the graphQL query errors', () => {
-    it('should render <ErrorMessage />', async() => {
-      mocks = [{
-        request: {
-          query: GET_COLLABORATOR
-        },
-        error: new Error('Something went wrong')
-      }];
-
-      wrapper = mount(
-        <BrowserRouter>
-          <MockedProvider mocks={mocks} addTypename={false}>
-            <CollaboratorView {...props} />
-          </MockedProvider>
-        </BrowserRouter>
-      );
-
-      await actions(wrapper, () => {
-        wrapper.update();
-        const actual = wrapper.containsMatchingElement(
-          <StickyNew>
-            <p>Sorry, something has gone wrong.</p>
-          </StickyNew>
-        );
-        expect(actual).to.equal(true);
-      });
-    });
+  it('should render <SingleEntityContainer />', () => {
+    const actual = wrapper.containsMatchingElement(
+      <SingleEntityContainer
+        baseUrl='/collaborators'
+        entityName='collaborator'
+        id={props.match.params.id}
+        component={Collaborator}
+        query={GET_COLLABORATOR}
+        mutation={DELETE_COLLABORATOR}
+        renderEditLink
+      />
+    );
+    expect(actual).to.equal(true);
   });
 
 });
