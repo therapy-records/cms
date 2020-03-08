b92804736b2350d67b005d4fc5cc86a7bb2664ca
ttbarnes <tony@tonybarnes.me>
2020-03-08 09:16:29 +0000

    tidy formFields imports

---
+++ src/formFields/collaborator-fields.js
@@ -0,0 +1,122 @@
+const COLLABORATOR_FIELDS = [
+  {
+    id: 'name',
+    type: 'text',
+    component: 'TextInput',
+    label: 'Name',
+    placeholder: 'Phil Collins',
+    required: true
+  },
+  {
+    id: 'role',
+    type: 'text',
+    component: 'TextInput',
+    label: 'What they do',
+    placeholder: 'e.g Saxophonist',
+    required: true
+  },
+  {
+    id: 'about',
+    type: 'text',
+    component: 'RichTextEditor',
+    title: 'About',
+    required: true,
+    placeholder: 'Born and raised in...'
+  },
+  {
+    id: 'avatarUrl',
+    type: 'text',
+    component: 'ImageUpload',
+    label: 'Avatar',
+    required: true,
+    ctaCopy: 'Drag & drop image',
+    minImageDimensions: {
+      width: 250,
+      height: 250
+    },
+    helpText: 'must be at least 250px by 250px'
+  },
+  {
+    id: 'collabOn',
+    type: 'arrayOfStrings',
+    component: 'TextInputsList',
+    fieldsetLegend: 'Collaborated on',
+    items: [
+      { value: '' },
+      { value: '' }
+    ]
+  },
+  {
+    id: 'urls',
+    type: 'arrayOfObjects',
+    component: 'TextInputsList',
+    heading: 'Links',
+    items: [
+      {
+        label: 'Website',
+        id: 'website',
+        type: 'text',
+        value: '',
+        placeholder: 'http://collaborator.com'
+      },
+      {
+        label: 'Facebook',
+        id: 'facebook',
+        type: 'text',
+        value: '',
+        placeholder: 'http://facebook.com/example'
+      },
+      {
+        label: 'Twitter',
+        id: 'twitter',
+        type: 'text',
+        value: '',
+        placeholder: 'http://twitter.com/example'
+      },
+      {
+        label: 'Instagram',
+        id: 'instagram',
+        type: 'text',
+        value: '',
+        placeholder: 'http://instagram.com/example'
+      },
+      {
+        label: 'SoundCloud',
+        id: 'soundcloud',
+        type: 'text',
+        value: '',
+        placeholder: 'http://soundcloud.com/example'
+      },
+      {
+        label: 'Bandcamp',
+        id: 'bandcamp',
+        type: 'text',
+        value: '',
+        placeholder: 'https://bandcamp.com'
+      },
+      {
+        label: 'Bio',
+        id: 'bio',
+        type: 'text',
+        value: '',
+        placeholder: 'http://website.com/example'
+      },
+      {
+        label: 'Email',
+        id: 'email',
+        type: 'email',
+        value: '',
+        placeholder: 'example@collaborator.com'
+      },
+      {
+        label: 'Phone',
+        id: 'phone',
+        type: 'tel',
+        value: '',
+        placeholder: '+441234567890'
+      }
+    ]
+  }
+];
+
+export default COLLABORATOR_FIELDS;
---
+++ src/formFields/press-fields.js
@@ -0,0 +1,43 @@
+const PRESS_FIELDS = [
+  {
+    id: 'author',
+    type: 'text',
+    component: 'TextInput',
+    label: 'Author',
+    placeholder: 'Sky News',
+    required: true
+  },
+  {
+    id: 'title',
+    type: 'text',
+    component: 'TextInput',
+    label: 'Title',
+    placeholder: 'An interview with Fiona Ross',
+    required: true
+  },
+  {
+    id: 'excerpt',
+    type: 'text',
+    component: 'TextInput',
+    label: 'Excerpt',
+    placeholder: 'Fiona has gone from strength to strength since...',
+    required: true
+  },
+  {
+    id: 'externalLink',
+    type: 'text',
+    component: 'TextInput',
+    label: 'URL',
+    placeholder: 'http://skynews.com/fiona-ross',
+    required: true
+  },
+  {
+    id: 'releaseDate',
+    type: 'text',
+    component: 'Datepicker',
+    label: 'Release Date',
+    required: true
+  }
+];
+
+export default PRESS_FIELDS;
