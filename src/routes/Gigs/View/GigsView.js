266f5f7d960c7e56300dff82abbf0ef1ec1f8f72
Bruno ADENIS <adenis.bruno@hotmail.com>
2020-03-23 18:57:01 +0000

    removed title

--- src/routes/Gigs/View/GigsView.js
+++ src/routes/Gigs/View/GigsView.js
@@ -37,8 +37,7 @@ const GigsView = ({
                 <div className='content-container'>
                     <div className='content'>
                         <div>
-                            <div className='small-tabs-container' style={{display: "inline"}}>
-                                {entityData.title && <h1>{entityData.title}</h1>}
+                            <div className='small-tabs-container'>
                                 {(entityData.venue && entityData.location && entityData.date) && <p>{entityData.venue}, {entityData.location}, {moment(new Date(entityData.date)).format('LT')}</p>}
                                 {entityData.ticketsUrl && <p><a href={entityData.ticketsUrl} target="_blank">{entityData.ticketsUrl}</a></p>}
                             </div>
