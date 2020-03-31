b57a185e7313cdff02cc53e5faf7974cc632eeba
Bruno ADENIS <adenis.bruno@hotmail.com>
2020-03-31 13:04:24 +0100

    added Gigs Create test route

--- src/routes.test.js
+++ src/routes.test.js
@@ -27,6 +27,7 @@ import CollaboratorsCreate from './routes/Collaborators/Create';
 import CollaboratorView from './routes/Collaborators/View';
 import CollaboratorEdit from './routes/Collaborators/Edit';
 import Gigs from './routes/Gigs';
+import GigCreate from './routes/Gigs/Create'
 import Videos from './routes/Videos';
 
 Enzyme.configure({ adapter: new Adapter() });
@@ -311,6 +312,20 @@ describe('Router', () => {
     );
     expect(actual).to.equal(true);
   });
+
+  it('should render gig create route', () => {
+    const actual = wrapper.containsMatchingElement(
+      <ProtectedRoute
+        path="/gigs/create"
+        component={GigCreate}
+        isAuth={props.isAuth}
+        onAuthCheck={props.onAuthCheck}
+        exact
+      />
+    );
+    expect(actual).to.equal(true);
+  });
+
 
   it('should render videos route', () => {
     const actual = wrapper.containsMatchingElement(
