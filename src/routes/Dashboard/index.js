efe54ec54a5ee60d531beb151918988b2d9a307d
ttbarnes <tony@tonybarnes.me>
2020-10-01 22:56:15 +0100

    remove temp component example

--- src/routes/Dashboard/index.js
+++ src/routes/Dashboard/index.js
@@ -1,20 +1,12 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import Stats from './components/Stats';
-import ImageUploadContainer from '../../components/FormElements/ImageUpload/ImageUploadContainer';
 import './style.css';
 
 const Dashboard = () => (
   <div className='container dashboard'>
 
     <h2>Welcome back <span className='wave'>👋</span></h2>
-
-    <ImageUploadContainer
-      minImageDimensions={{
-        width: 400,
-        height: 400
-      }}
-    />
 
     <div className='ctas'>
       <Link to='news/create' className='btn'>Create News</Link>
